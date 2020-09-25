b8949c1de53c78dda849e7e38627fe201dab598f
SvetlankaK <svetlanaka17@hmail.com>
2020-09-25 14:32:55 +0300

    почему юзер не инициализируется? или только роли? или зачем мы живем?

--- src/app/domain/user.ts
+++ src/app/domain/user.ts
@@ -2,18 +2,18 @@ import {Role} from "./role";
 
 export class User {
   userLogin: string;
-  password: string;
+  password?: string;
   name: string;
-  surname: string;
-  dateOfBirth: string;
-  email: string;
+  surname?: string;
+  dateOfBirth?: string;
+  email?: string;
   userId: number;
-  salary: number;
+  salary?: number;
   role: Role[];
   roleValues?: string[];
   token?: string;
 
-  constructor(id: number, name: string, userLogin: string, password: string, surname: string, dateOfBirth: string, email: string, salary: number, role: Role[]) {
+  constructor(id: number, name: string, userLogin: string,  role: Role[]) {
   }
 
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -4,6 +4,7 @@ import {UserService} from './user.service';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {map} from 'rxjs/operators';
+import {Role} from "../domain/role";
 
 @Injectable({
   providedIn: 'root'
@@ -23,11 +24,19 @@ export class AuthService {
   login(userLogin: string, password: string): Observable<boolean> {
     return this.http.post<any>(`http://localhost:8090/api/auth/signin`, {password, userLogin}).pipe(map(resp => {
         localStorage.setItem('token', resp.token);
-        console.log(resp)
-        this.loggedUser.role = [];
-        this.loggedUser.role = [{roleName: resp.role}];
-        this.loggedUser.userLogin = resp.userLogin;
-        this.loggedUser.name = resp.name;
+        console.log(resp);
+        let roles: Role[];
+        for (let i = 0; i < resp.roles.length; i++) {
+          switch (resp.roles[i]) {
+            case resp.roles[i] === "admin":
+              roles.push({roleName: "admin", id: 2})
+              break;
+            case resp.roles[i] === "user":
+              roles.push({roleName: "user", id: 1})
+              break;
+          }
+        }
+        this.loggedUser = new User(resp.id, resp.name, resp.userLogin, roles);
         console.log(this.loggedUser)
         return true;
       }
